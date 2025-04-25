5ac1d808c8aa35a70119e3f15d04086850390999
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-04-25 16:57:23 -0400

    Add OrderBook type, update MarketState type to MarketStoreState type

--- src/types/marketData.ts
+++ src/types/marketData.ts
@@ -5,13 +5,17 @@ export type Order = {
   size: number;
 };
 
-export type MarketState = {
-    ticker: TickerData | null;
-    // Record<string, number>; // e.g., { AAPL: 187.23 }
-    orderBook: {
-      bids: Order[],
-      asks: Order[],
-    };
-    updateTicker: (ticker: TickerData) => void;
-    updateBook: (bids: Order[], asks: Order[]) => void;
+export type OrderBook = {
+  bids: Order[];
+  asks: Order[];
+}
+
+export type MarketStoreState = {
+  ticker: TickerData | null; // e.g., { AAPL: 187.23 }
+  orderBook: {
+    bids: Order[],
+    asks: Order[],
   };
+  updateTicker: (ticker: TickerData) => void;
+  updateBook: (bids: Order[], asks: Order[]) => void;
+};
