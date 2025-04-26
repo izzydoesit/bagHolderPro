a08cdea4cd0216afb230c99c6dafa62c6ce622e2
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-04-26 15:10:24 -0400

    Add AccountOverview to Dashboard layout

--- src/components/Layout/DashboardLayout.tsx
+++ src/components/Layout/DashboardLayout.tsx
@@ -1,17 +1,27 @@
 import MockStockTicker from "@/mocks/MockStockTicker";
+// import StockTicker from "../StockTicker/StockTicker";
+import AccountOverview from "@/components/Account/AccountOverview";
 import TradePanel from "@/components/TradePanel/TradePanel";
+import MockOrderBook from "../../mocks/MockOrderBook";
+import OrderBook from "../OrderBook/OrderBook";
+
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-black text-white grid grid-rows-[auto_1fr_auto] gap-4 p-4">
       <header className="flex justify-between items-center text-lg font-semibold">
         <span>⏱️ 09:30:05</span>
+        {/* <StockTicker /> */}
         <MockStockTicker />
-        <span>Account: +$2,540</span>
+        <span>
+          <AccountOverview />
+        </span>
       </header>
 
       <main className="grid grid-cols-[1fr_2fr_1fr] gap-4">
         {children}
+        {/* <MockOrderBook /> */}
+        {/* <OrderBook /> */}
       </main>
 
       <footer className="bg-gray-900 p-4 rounded-lg text-center">
